e9bb3d51900c0c18cc1978ea7496f90f40ebc915
Nicolas vauché <hello@nicolasvauche.net>
2025-02-17 12:33:51 +0100

    Front Office: Register

---
+++ src/Controller/FrontOffice/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\FrontOffice;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RegisterController extends AbstractController
+{
+    #[Route('/nouveau-joueur', name: 'app_front_office_register')]
+    public function index(Request                     $request,
+                          UserPasswordHasherInterface $userPasswordHasher,
+                          Security                    $security,
+                          EntityManagerInterface      $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword))
+                ->setActive(true);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', "Bienvenue à toi, {$user->getName()} !");
+
+            return $security->login($user, 'form_login', 'main');
+        }
+
+        return $this->render('front_office/register/index.html.twig', [
+            'form' => $form,
+        ], new Response(null, $form->isSubmitted() && !$form->isValid() ? 422 : 200));
+    }
+}

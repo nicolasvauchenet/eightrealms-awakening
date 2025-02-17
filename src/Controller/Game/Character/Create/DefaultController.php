c658e549118c4713c794eeb8fd33ba0cce3a469e
Nicolas vauché <hello@nicolasvauche.net>
2025-02-17 18:10:16 +0100

    Character: PreGenerated Grid

--- src/Controller/Game/Character/Create/DefaultController.php
+++ src/Controller/Game/Character/Create/DefaultController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller\Game\Character\Create;
 
+use App\Repository\Character\PreGeneratedRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class DefaultController extends AbstractController{
+final class DefaultController extends AbstractController
+{
     #[Route('/nouveau-personnage', name: 'app_game_character_create_home')]
-    public function index(): Response
+    public function index(PreGeneratedRepository $preGeneratedRepository): Response
     {
         return $this->render('game/character/create/default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'characters' => $preGeneratedRepository->findAll(),
         ]);
     }
 }

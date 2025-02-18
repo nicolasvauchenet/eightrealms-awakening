3da27258a6a03263059e516bf6f6b37c402fb305
Nicolas vauché <hello@nicolasvauche.net>
2025-02-18 11:11:04 +0100

    PreGenerated Sheet

---
+++ src/Controller/Game/Character/Sheet/PreGeneratedController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Game\Character\Sheet;
+
+use App\Entity\Character\PreGenerated;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PreGeneratedController extends AbstractController
+{
+    #[Route('/details-du-personnage/{slug}', name: 'app_game_character_sheet_pregenerated')]
+    public function index(PreGenerated $character): Response
+    {
+        return $this->render('game/character/sheet/pregenerated/index.html.twig', [
+            'character' => $character,
+        ]);
+    }
+}

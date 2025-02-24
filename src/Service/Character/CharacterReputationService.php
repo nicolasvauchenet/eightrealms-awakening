a6cf2f9af6436efbebd88762b7391eff90f87dde
Nicolas vauché <hello@nicolasvauche.net>
2025-02-24 19:38:24 +0100

    Reputation fix

--- src/Service/Character/CharacterReputationService.php
+++ src/Service/Character/CharacterReputationService.php
@@ -19,11 +19,17 @@ readonly class CharacterReputationService
             $characterRaceAttitude === 'amical' && $npcRaceAttitude === 'amical' => 2,
             $characterRaceAttitude === 'hostile' && $npcRaceAttitude === 'amical' => -2,
             $characterRaceAttitude === 'hostile' && $npcRaceAttitude === 'hostile' => 1,
+            $characterRaceAttitude === 'hostile' && $npcRaceAttitude === 'neutre' => -1,
             default => 0,
         };
 
-        if($characterProfessionAttitude === 'amical' && $npcProfessionAttitude === 'amical') $reputation += 2;
-        else if($characterProfessionAttitude === 'hostile' && $npcProfessionAttitude === 'amical') $reputation -= 1;
+        $reputation += match (true) {
+            $characterProfessionAttitude === 'amical' && $npcProfessionAttitude === 'amical' => 2,
+            $characterProfessionAttitude === 'hostile' && $npcProfessionAttitude === 'amical' => -2,
+            $characterProfessionAttitude === 'hostile' && $npcProfessionAttitude === 'hostile' => 1,
+            $characterProfessionAttitude === 'hostile' && $npcProfessionAttitude === 'neutre' => -1,
+            default => 0,
+        };
 
         if($character->getCharisma() >= 12) $reputation += 1;
         else if($character->getCharisma() <= 9) $reputation -= 1;

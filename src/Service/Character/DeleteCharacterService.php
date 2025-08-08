94a128bc1967e6f4cda0d6458e6db0692b51f1a6
Nicolas vauché <hello@nicolasvauche.net>
2025-08-08 17:45:02 +0200

    Fix: Delete Player Character

--- src/Service/Character/DeleteCharacterService.php
+++ src/Service/Character/DeleteCharacterService.php
@@ -3,21 +3,24 @@
 namespace App\Service\Character;
 
 use App\Entity\Character\Player;
+use App\Entity\Reward\PlayerReward;
+use App\Entity\Reward\Reward;
 use App\Service\FileUploaderService;
 use Doctrine\ORM\EntityManagerInterface;
 
 readonly class DeleteCharacterService
 {
-    public function __construct(private EntityManagerInterface $entityManager,
-                                private FileUploaderService    $fileUploaderService)
-    {
-
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private FileUploaderService $fileUploaderService
+    ) {
     }
 
     public function deleteCharacter(Player $character): void
     {
         $this->deleteCharacterItems($character);
         $this->deleteCharacterSpells($character);
+        $this->deleteCharacterRewards($character);
 
         $this->fileUploaderService->remove('character', $character->getPicture());
         $this->entityManager->remove($character);
@@ -26,7 +29,7 @@ readonly class DeleteCharacterService
 
     private function deleteCharacterItems(Player $character): void
     {
-        foreach($character->getCharacterItems() as $characterItem) {
+        foreach ($character->getCharacterItems() as $characterItem) {
             $character->removeCharacterItem($characterItem);
             $this->entityManager->remove($characterItem);
         }
@@ -34,9 +37,18 @@ readonly class DeleteCharacterService
 
     private function deleteCharacterSpells(Player $character): void
     {
-        foreach($character->getCharacterSpells() as $characterSpell) {
+        foreach ($character->getCharacterSpells() as $characterSpell) {
             $character->removeCharacterSpell($characterSpell);
             $this->entityManager->remove($characterSpell);
+        }
+    }
+
+    private function deleteCharacterRewards(Player $character): void
+    {
+        foreach (
+            $this->entityManager->getRepository(PlayerReward::class)->findBy(['player' => $character]) as $playerReward
+        ) {
+            $this->entityManager->remove($playerReward);
         }
     }
 }
